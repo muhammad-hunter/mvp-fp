a25c96a229ccd06ccdb5dacf9f2ed3fadb92c260
mukhammadlatifl1280 <73484322+mukhammadlatifl1280@users.noreply.github.com>
2023-04-16 09:08:59 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var priceTxt: UITextField!
+    @IBOutlet weak var salesTaxTxt: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        totalLabel.text = ""
+    }
+
+    @IBAction func calculateTotalPrice(_ sender: Any) {
+        let price = Double(priceTxt.text!)!
+        let salesTax = Double(salesTaxTxt.text!)!
+        
+        let totalSalesTax = price * salesTax
+        
+        let totalPrice = price + totalSalesTax
+        
+        totalLabel.text = "$\(totalPrice)"
+        
+    }
+    
+}
+
